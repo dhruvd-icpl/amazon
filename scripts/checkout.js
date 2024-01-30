73a02e17def22c2fdf2726da6d997916a95a2a73
dhruvd-icpl <dhruvd@infopercept.com>
2024-01-30 10:23:24 +0530

    delete products from cart when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
-import {cart} from '../data/cart.js';
+import {cart,  removeFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import {formateCurrency} from'./utils/money.js';
+
 
 let cartSummaryHTML = '';
 
@@ -43,7 +44,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary  js-delete-link" data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -100,3 +101,11 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click',() => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    console.log(cart);
+    }); 
+  });
